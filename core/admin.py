138b11b001f6d72242fc81c93289c129be05a7cd
YisusBk <masterbk12@gmail.com>
2020-09-13 02:44:13 -0300

    commitestable

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,6 @@
-""" from django.contrib import admin
+from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Vehiculo)
 admin.site.register(Transportista)
-<<<<<<< HEAD
-admin.site.register(Producto) """
-=======
 admin.site.register(Producto)
-
->>>>>>> f00e41a75ed433fde03eb78c4ebfd43562aafc45
