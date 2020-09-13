29fc585ff729acaff00d1dca03ae2020cf9671c8
Manuel Gonzalez <mngc98@gmail.com>
2020-09-13 06:10:52 -0300

    creacion login y cambios en  settings,urls

--- MaipoGrande/urls.py
+++ MaipoGrande/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+admin.site.site_header="Administración de FrutOnline"
+admin.site.index_title="Módulos de administración"
+admin.site.site_title="FrutOnline"
--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from .serializers import ProductoSerializer
 def home(request):
     return render(request,'core/home.html')
 
+
 def productos(request):
     return render(request,'core/productos.html' )
 
