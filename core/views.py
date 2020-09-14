1d7f9fe225b277d70f7d5dbe9ad0e44101ca1424
Manuel Gonzalez <mngc98@gmail.com>
2020-09-13 22:14:52 -0300

    Login, settis, urls

--- core/views.py
+++ core/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
 from .serializers import ProductoSerializer
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def home(request):
     return render(request,'core/home.html')
-
 
 def productos(request):
     return render(request,'core/productos.html' )
