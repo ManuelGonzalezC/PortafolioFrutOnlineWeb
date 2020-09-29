73b65a7dde8d715ea55c33dfb803716b0cb39818
Cristofer Sagredo <cristofersagredo@gmail.com>
2020-09-28 23:37:38 -0300

    login trans incompleto

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('list_subastas/', list_subastas, name = "list_subastas"),
     path('ingresar_subasta/', ingresar_subasta, name = "ingresar_subasta"),
     path('mod_subastas/<id>/', mod_subasta, name = "mod_subastas"),
-    path('eliminar_subasta/<id>/', eliminar_subasta, name = "eliminar_subasta")
+    path('eliminar_subasta/<id>/', eliminar_subasta, name = "eliminar_subasta"),
 ]
