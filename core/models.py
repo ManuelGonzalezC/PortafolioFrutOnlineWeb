2c2d08cf9ca902900aba48b0ed41eb2c5b87847a
Cristofer Sagredo <cristofersagredo@gmail.com>
2020-11-14 18:06:31 -0300

    nada nuevo 5

--- core/models.py
+++ core/models.py
@@ -6,8 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.contrib.auth.models import User
-
+#from django.contrib.auth.models import User
 
 class Adminc(models.Model):
     username = models.CharField(primary_key=True, max_length=255)
@@ -92,7 +91,7 @@ class ClienteExterno(models.Model):
     telefono = models.BigIntegerField()
     email = models.CharField(max_length=100)
     id_pais = models.ForeignKey('Pais', models.DO_NOTHING, db_column='id_pais')
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #user = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
